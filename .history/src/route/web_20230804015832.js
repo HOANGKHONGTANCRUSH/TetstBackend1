f50837e5339b77c6026fb126894eabc2639deffc
unknown <hoangpham.04042002@gmail.com>
2023-08-04 03:10:26 +0700

    nodejs lan 1

---
+++ .history/src/route/web_20230804015832.js
@@ -0,0 +1,28 @@
+import express from "express";
+import homeController from "../controllers/homeController";
+import userControllers from "../controllers/userControllers"
+
+let router = express.Router();
+
+let initWebRoutes = (app) => {
+    router.get('', homeController.getHomePage);
+    router.get('/about', homeController.getAbuotPage);
+    router.get('/crud', homeController.getCRUD);
+    router.post('/post-crud', homeController.postCRUD);
+    router.get('/get-crud', homeController.displayGetCRUD);
+    router.get('/edit-crud', homeController.getEditCRUD);
+    router.post('/put-crud', homeController.putCRUD);
+    router.get('/delete-crud', homeController.deleteCRUD);
+    router.post('/api/login', userControllers.handleLogin)
+    router.get('/api/get-all-users', userControllers.handleGetAllUsers)
+    router.put('/api/edit-user', userControllers.handleCreateNewUser)
+    router.post('/api/create-new-user',userControllers.handleCreateNewUser)
+    
+
+
+
+
+    return app.use("/", router);
+}
+
+module.exports = initWebRoutes;
